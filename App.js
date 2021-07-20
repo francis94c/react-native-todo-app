ba67014f6b8339d9e5b19cc626451051f59c30d7
Francis Ilechukwu <francis94c@gmail.com>
2021-07-20 13:01:31 +0100

    Code Format.

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
-import appStyles from './styles/main';
-import colors from './styles/colors';
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import appStyles from "./styles/main";
+import colors from "./styles/colors";
 
 export default function App() {
   return (
     <SafeAreaView style={appStyles.container}>
       <Text style={styles.titleText}>March 9, 2020</Text>
       <Text style={styles.summaryText}>5 incomplete 5 complete</Text>
-      <View style={{borderBottomColor: colors.secondaryGrey, borderBottomWidth: 1 }}></View>
-      <View style={{marginTop: 16}}>
+      <View
+        style={{
+          borderBottomColor: colors.secondaryGrey,
+          borderBottomWidth: 1,
+        }}
+      ></View>
+      <View style={{ marginTop: 16 }}>
         <Text style={styles.sectionTitleText}>Incomplete</Text>
       </View>
     </SafeAreaView>
@@ -20,15 +25,15 @@ const styles = StyleSheet.create({
   titleText: {
     ...appStyles.bigText,
     fontWeight: "bold",
-    marginBottom: 8
+    marginBottom: 8,
   },
   summaryText: {
     ...appStyles.smallText,
     color: colors.secondaryGrey,
-    marginBottom: 16
+    marginBottom: 16,
   },
   sectionTitleText: {
     ...appStyles.text,
     fontWeight: "bold",
-  }
+  },
 });
