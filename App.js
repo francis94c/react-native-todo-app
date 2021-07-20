04d9b650e53d420d03d2d1bca4768a235ac1f1f4
Francis Ilechukwu <francis94c@gmail.com>
2021-07-20 06:37:07 +0100

    Date Text View and Summary.

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import appStyles from './styles/main';
+import colors from './styles/colors';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={appStyles.container}>
+      <Text style={styles.titleText}>March 9, 2020</Text>
+      <Text style={styles.summaryText}>5 incomplete 5 complete</Text>
+      <View style={{borderBottomColor: colors.secondaryGrey, borderBottomWidth: 1 }}></View>
+      <View style={{marginTop: 16}}>
+        <Text style={styles.sectionTitleText}>Incomplete</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
+  titleText: {
+    ...appStyles.bigText,
+    fontWeight: "bold",
+    marginBottom: 8
   },
-  text: {
-    color: '#fff',
+  summaryText: {
+    ...appStyles.smallText,
+    color: colors.secondaryGrey,
+    marginBottom: 16
+  },
+  sectionTitleText: {
+    ...appStyles.text,
+    fontWeight: "bold",
   }
 });
